9169fe88b370fe29758d329ab1f77b5a52bdacc8
Nandi Wong <nandi@kintohub.com>
2020-04-15 16:53:03 +0800

    add logger

--- run.js
+++ run.js
@@ -1,14 +1,29 @@
 var restify = require('restify');
+const Logger = require('bunyan');
+
+const logger = new Logger.createLogger({
+  name: 'app-name',
+  serializers: {
+      req: Logger.stdSerializers.req
+  }
+})
 
 function respond(req, res, next) {
   res.send('hello ' + req.params.name);
   next();
 }
 
-var server = restify.createServer();
+var server = restify.createServer({
+  log: logger
+});
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
-server.listen(8080, function() {
+server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
+});
+
+server.pre((req, res, next) => {
+  req.log.info({ req }, 'REQUEST');
+  next();
 });
